533634e9b89c04dae6841fc08e0dfdfc72b71080
Miguel <miguelmds925@gmail.com>
2024-05-17 18:48:17 -0300

    Resolvendo o desafio

---
+++ TestePhp/desafio.php
@@ -0,0 +1,16 @@
+<?php 
+  $personagem = [
+    $nome = 'Bonieky',
+    $idade = 90,
+    $atributos = [
+        $forca = 60,
+        $agilidade = 80,
+        $destreza = 50
+    ],
+    $vida = 1000,
+    $mana = 928
+  ];
+
+  echo "NOME: ".$personagem[0]."</br>";
+  echo "FORCA: ".$personagem[2][0]."</br>";
+  echo "VIDA: ".$personagem[3];
