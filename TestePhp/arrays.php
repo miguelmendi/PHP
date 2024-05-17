243bdba8b22628b4c77ed188c24e12e77518592c
Miguel <miguelmds925@gmail.com>
2024-05-17 18:44:22 -0300

    aprendendo sobre Array Spread

--- TestePhp/arrays.php
+++ TestePhp/arrays.php
@@ -1,9 +1,20 @@
 <?php 
-  $ingredientes = [
+  $bolo1 = [
     "açucar",
     "farinha de trigo",
     "ovo",
     "leite",
     "fermento em pó"];
 
-  echo $ingredientes[2];
+  $bolo2 = [
+    "vasilha",
+    "agua morna"
+    ...$bolo1;
+    "corante";
+  ];
+
+  $lista1 = ["binieky", "pedro", "joão"];
+  $lista2 = ["patricia", "fabricia", "melissa"];
+  $lista3 = [...$lista1, ...$lista2];
+
+  print_r($lista3);
