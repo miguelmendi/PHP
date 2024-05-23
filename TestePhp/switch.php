aec679798a9d00a4af4c57adb998c6a819c8f080
Miguel <miguelmds925@gmail.com>
2024-05-23 18:41:29 -0300

    Vendo o uso do switch

---
+++ TestePhp/switch.php
@@ -0,0 +1,16 @@
+<?php
+  $tipo = "texto";
+
+  switch ($tipo) {
+    case 'foto':
+      echo "exibindo Foto";
+      break;
+    
+    case "video":
+      echo "exibindo video"
+      break;
+
+      case "texto":
+        echo "exibindo texto"
+        break;
+  }
